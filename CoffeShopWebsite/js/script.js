6e83c3eac104ec377853fdc652f8f27c83975732
Rafael Gamarra <rafaelgamas@gmail.com>
2022-05-07 15:13:21 -0400

    finishing frontend demo

--- CoffeShopWebsite/js/script.js
+++ CoffeShopWebsite/js/script.js
@@ -1,7 +1,32 @@
-import $ from "jquery";
+"use Strict";
+function init() {
+  let navbar = document.querySelector(".navbar");
 
-let navbar = $("#navbar");
-debugger;
-document.querySelector("#menu-btn").onclick = () => {
-  navbar.classList.toggle("active");
-};
+  document.querySelector("#menu-btn").onclick = () => {
+    navbar.classList.toggle("active");
+    searchForm.classList.remove("active");
+    cartItem.classList.remove("active");
+  };
+
+  let searchForm = document.querySelector(".search-form");
+
+  document.querySelector("#search-btn").onclick = () => {
+    searchForm.classList.toggle("active");
+    navbar.classList.remove("active");
+    cartItem.classList.remove("active");
+  };
+
+  let cartItem = document.querySelector(".cart-items-container");
+
+  document.querySelector("#cart-btn").onclick = () => {
+    cartItem.classList.toggle("active");
+    navbar.classList.remove("active");
+    searchForm.classList.remove("active");
+  };
+
+  window.onscroll = () => {
+    navbar.classList.remove("active");
+    searchForm.classList.remove("active");
+    cartItem.classList.remove("active");
+  };
+}
